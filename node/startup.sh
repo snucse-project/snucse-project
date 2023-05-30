34bce9cdfd7b6d43e4715e1406bad9054fd3cbf9
yeonsumia <yeonsumia@snu.ac.kr>
2023-05-30 15:11:47 +0900

    chore: not initialization

--- node/startup.sh
+++ node/startup.sh
@@ -6,8 +6,8 @@ sleep 10
 echo $(netstat -lntp | grep 3000) # for ubuntu
 # echo $(netstat -vanp tcp | grep 3000) # for mac
 
-curl -d '{ "path": "../tmp/data/enwiki_test_articles.json" }' \
-     -H "Content-Type: application/json" \
-     -X POST localhost:3000/init
-sleep 300
-curl -X GET localhost:3000/article/Anarchism # test query
+# curl -d '{ "path": "../tmp/data/enwiki_test_articles.json" }' \
+#      -H "Content-Type: application/json" \
+#      -X POST localhost:3000/init
+# sleep 600
+# curl -X GET localhost:3000/article/Anarchism # test query
