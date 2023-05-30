566077b3e4ed04bb5cd6a635367dcbee6053619b
yeonsumia <yeonsumia@snu.ac.kr>
2023-05-30 19:56:08 +0900

    chore: poststart script

--- node/startup.sh
+++ node/startup.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-./node_modules/.bin/pm2 start app.js
 sleep 10
 
 echo $(netstat -lntp | grep 3000) # for ubuntu
 # echo $(netstat -vanp tcp | grep 3000) # for mac
 
-# curl -d '{ "path": "../tmp/data/enwiki_test_articles.json" }' \
-#      -H "Content-Type: application/json" \
-#      -X POST localhost:3000/init
-# sleep 600
-# curl -X GET localhost:3000/article/Anarchism # test query
-
-exit 0
+curl -d '{ "path": "../tmp/data/enwiki_test_articles.json" }' \
+     -H "Content-Type: application/json" \
+     -X POST localhost:3000/init
+sleep 30
+curl -X GET localhost:3000/article/Anarchism # test query
