cee9852ffcf6219ad6afd228da87b714666f994a
yeonsumia <yeonsumia@snu.ac.kr>
2023-05-30 11:00:28 +0900

    chore: startup script for pods

--- node/startup.sh
+++ node/startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-npm start
 curl -d '{ "path": "../tmp/data/enwiki_test_articles.json" }' \
      -H "Content-Type: application/json" \
      -X POST localhost:3000/init
+curl -X GET localhost:3000/article/Anarchism # test query
