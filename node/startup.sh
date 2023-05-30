566f7c58cc519f18edced2287d3725c497e33a48
yeonsumia <yeonsumia@snu.ac.kr>
2023-05-30 12:37:47 +0900

    chore: pm2 directory

--- node/startup.sh
+++ node/startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pm2 start app.js
+./node_modules/.bin/pm2 start app.js
 curl -d '{ "path": "../tmp/data/enwiki_test_articles.json" }' \
      -H "Content-Type: application/json" \
      -X POST localhost:3000/init
