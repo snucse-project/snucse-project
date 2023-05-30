76c84f35b7cd8e4ba8357605aa478a4710dfcd5f
yeonsumia <yeonsumia@snu.ac.kr>
2023-05-30 12:46:38 +0900

    chore: localhost connection issue

--- node/startup.sh
+++ node/startup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 ./node_modules/.bin/pm2 start app.js
+echo $(netstat -lntp | grep 3000)
+
 curl -d '{ "path": "../tmp/data/enwiki_test_articles.json" }' \
      -H "Content-Type: application/json" \
      -X POST localhost:3000/init
