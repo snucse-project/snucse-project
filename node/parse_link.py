0dd85e072296d9a8c37b1521c2f75f3174565268
yeonsumia <yeonsumia@snu.ac.kr>
2023-06-05 12:49:57 +0900

    feat: parse modules in each pod

--- node/parse_link.py
+++ node/parse_link.py
@@ -2,6 +2,8 @@
 # [{"title": string, "contributor": string, "start": int, "end": int}, {}, {}, ...]
 import json
 import sys
+import re
+from collections import Counter
 
 file_path = sys.argv[1]
 page_num = sys.argv[2]
@@ -16,17 +18,14 @@ with open(file_path, mode='r', encoding='utf8') as input_file:
 		if ("\"title\"" in line) and (not title_found):
 			title = line.split(':')[1].strip().rstrip(',').strip('\"')
 			title_found = True
-		if ("\"username\"" in line or "\"ip\"" in line) and title_found:
-			contributor = line.split(':')[1].strip().rstrip(',').strip('\"')
 		if ("\"#text\":" in line) and title_found:
-			start += 25
-			end = input_file.tell()-1
-			parsed_data.append({"title": title, "contributor": contributor, "start": start, "end": end})
+			linked_titles = re.findall(r"(?<=\[\[).+?(?=\]\]|\|.*\]\]|#.*\]\])", line)
+			link_dict = dict(Counter(linked_titles))
+			parsed_data.append({"title": title, "links": sorted(link_dict, key=link_dict.get, reverse=True)[:5]})
 			title_found = False
 
-		start = input_file.tell()
 		line = input_file.readline()
 
 print(json.dumps(parsed_data, indent=4))
-with open(f'parsed_{page_num}.json', mode='w', encoding='utf8') as output_file:
+with open(f'link_parsed_{page_num}.json', mode='w', encoding='utf8') as output_file:
    json.dump(parsed_data, output_file, indent=4)
