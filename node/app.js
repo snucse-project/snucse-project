5bc7b21ee3fbcb67836702f74a8c5fa6f0d4d5e6
yeonsumia <yeonsumia@snu.ac.kr>
2023-06-08 08:35:25 +0900

    revert

--- node/app.js
+++ node/app.js
@@ -35,7 +35,7 @@ function comparator(a, b) {
 const bptree = new BTree(comparator);
 /* select cache type */
 // const titleCache = new cache.CacheLRU(1000000);  // @param memory limit
-const titleCache = new cache.CacheClock(2500000);  // @param memory limit
+const titleCache = new cache.CacheClock(1000000);  // @param memory limit
 const indexRouter = require('./routes/index');
 
 module.exports.bptreeInstance = bptree;
