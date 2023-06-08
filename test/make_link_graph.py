e0a8c033a7575fff09d4643c99e391a09159cccf
yeonsumia <yeonsumia@snu.ac.kr>
2023-06-08 20:30:50 +0900

    feat: make pattern script

--- test/make_link_graph.py
+++ test/make_link_graph.py
@@ -23,24 +23,25 @@ if __name__ == "__main__":
     address = sys.argv[1]
     url_prefix = f"http://{address}/article"
 
-    graph = dict()
-    urls = []
-    with open('data/link_parsed.json', 'r') as articles:
-        articles = json.load(articles)
+    for file_num in range(1, 11):
+        graph = dict()
+        urls = []
+        with open(f'data/parsed_link/enwiki-latest-pages-articles_{file_num}_parsed_link.json', 'r') as articles:
+            articles = json.load(articles)
 
-    titles = set()
-    for a in articles:
-        titles.add(a['title'])
+        titles = set()
+        for a in articles:
+            titles.add(a['title'])
 
-    for a in articles:
-        graph[a['title']] = [art for art in a['links'] if art in titles]
+        for a in articles:
+            graph[a['title']] = [art for art in a['links'] if art in titles]
 
-    called = set()
-    with open(os.path.join(here, 'link_url.txt'), mode='w') as writer:
-        for title in titles:
-            title_queries = DFS(graph, title)
-            for t in title_queries:
-                if t not in called:
-                    url = url_prefix + f"/{t}\n"
-                    writer.write(url)
-            called.update(title_queries)
+        called = set()
+        with open(os.path.join(here, f'link_urls/url_{file_num}.txt'), mode='w', encoding='utf8') as writer:
+            for title in titles:
+                title_queries = DFS(graph, title)
+                for t in title_queries:
+                    if t not in called:
+                        url = url_prefix + f"/{t}\n"
+                        writer.write(url)
+                called.update(title_queries)
--- test/make_zipf_url.js
+++ test/make_zipf_url.js
@@ -1,11 +1,18 @@
 const { ZipfGenerator } = require('./zipf.js');
 const { ArgumentParser } = require('argparse');
 var fs = require('fs');
-D = 27379; // number of documents
-R = D; // number of requests
+
 const parser = new ArgumentParser({});
 parser.add_argument('-a', '--address');
-address = parser.address
+parser.add_argument('-n', '--number');
+parser.add_argument('-d', '--document');
+parsed_args = parser.parse_args()
+D = parsed_args.document; // number of documents
+R = 3*D; // number of requests
+address = parsed_args.address
+file_num = parsed_args.number
+console.log(address)
+console.log(file_num)
 const zipfGen = new ZipfGenerator(D); 
 
 const cntArr = Array.from({ length: D }, (_, index) => 0);
@@ -15,18 +22,18 @@ for(let i=0; i<R; i++){
 
 console.log(cntArr)
 
-fs.open('test/zipf_url.txt','w',function(err,fd){
+fs.open('test/zipf_urls/url_'+file_num+'.txt','w',function(err,fd){
 	if (err) throw err;
  	console.log('file open complete');
 });
 
-var urls = fs.readFileSync('test/url.txt','utf8').split("\n")
+var urls = fs.readFileSync('test/urls/url_'+file_num+'.txt','utf8').split("\n")
 console.log(urls[0])
 
 for(let i=0; i<D; i++){
     var query = urls[i] + '\n'
     console.log(cntArr[i])
     for(let j=0;j<cntArr[i];j++){
-        fs.appendFileSync('test/zipf_url.txt', query, 'utf8', { 'flags': 'a+' })
+        fs.appendFileSync('test/zipf_urls/url_'+file_num+'.txt', query, 'utf8', { 'flags': 'a+' })
     }
 }
