4bd5b941e01cc287f2bd71763a96e185a120f4bd
yeonsumia <yeonsumia@snu.ac.kr>
2023-06-09 16:14:08 +0900

    feat: demo mode in traffic generator

--- test/multithread_api_call.py
+++ test/multithread_api_call.py
@@ -25,8 +25,8 @@ def getStatus(ourl):
         return "error", ourl
 
 def doSomethingWithResult(status, url):
-    pass
-    # print(status, url)
+    # pass
+    print(url)
 
 
 def parse_url(url):
@@ -41,6 +41,7 @@ if __name__ == "__main__":
     here = pathlib.Path(__file__).parent
     parser = argparse.ArgumentParser()
     parser.add_argument('-t', '--type', choices=["random", "zipf", "link"])
+    parser.add_argument('-m', '--mode', choices=["demo", "test"])
     args = parser.parse_args()
 
     thread_count = 1000 #32000 
@@ -57,8 +58,8 @@ if __name__ == "__main__":
             import random
             # while True:
             urls = []
-            for i in range(1, 11):
-                for url in open(os.path.join(here, f'urls/url_{i}.txt')):
+            for i in range(1, 11) if args.mode == "test" else range(1, 2):
+                for url in open(os.path.join(here, f'urls/url_{i}.txt' if args.mode == "test" else f'urls/demo_url_{i}.txt')):
                     urls.append(parse_url(url))
         
             random_urls = []
@@ -66,20 +67,25 @@ if __name__ == "__main__":
                 random_urls.append(random.choice(urls))
             start = time.time()
             # print(f"total urls: {len(random_urls)}")
-            for url in random_urls[:len(random_urls)//100]:
-                q.put(url)
+            if args.mode == "test":
+                for url in random_urls[:len(random_urls)//100]:
+                    q.put(url)
+            else:
+                while True:
+                    for url in random_urls:
+                        q.put(url)
             q.join()
             end = time.time()
-            print(f"total urls: {len(random_urls[:len(random_urls)//100])}")
+            # print(f"total urls: {len(random_urls[:len(random_urls)])}")
         elif args.type == "zipf":
             import random
             total_zipf_urls = []
-            for i in range(1, 11):
+            for i in range(1, 11) if args.mode == "test" else range(1, 2):
                 zipf_urls = []
-                for url in open(os.path.join(here, f'zipf_urls/url_{i}.txt')):
+                for url in open(os.path.join(here, f'zipf_urls/url_{i}.txt' if args.mode == "test" else f'zipf_urls/demo_url_{i}.txt')):
                     zipf_urls.append(parse_url(url))
                 random.shuffle(zipf_urls)
-                total_zipf_urls.extend(zipf_urls[:len(zipf_urls)//100])
+                total_zipf_urls.extend(zipf_urls[:len(zipf_urls)//100] if args.mode == "test" else zipf_urls[:len(zipf_urls)//10])
             start = time.time()
             print(f"total urls: {len(total_zipf_urls)}")
             for url in total_zipf_urls:
@@ -88,18 +94,18 @@ if __name__ == "__main__":
             end = time.time()
         elif args.type == "link":
             total_link_urls = []
-            for i in range(1, 11):
+            for i in range(1, 11) if args.mode == "test" else range(1, 2):
                 link_urls = []
                 for url in open(os.path.join(here, f'link_urls/url_{i}.txt')):
                     link_urls.append(parse_url(url))
-                total_link_urls.append(link_urls[:5000])
+                total_link_urls.append(link_urls[:5000] if args.mode == "test" else link_urls)
             start = time.time()
             # total_link_urls = np.array(total_link_urls)
             # print(total_link_urls)
             print(f"total urls: {len(np.array(total_link_urls).flatten())}")
             while len(np.array(total_link_urls).flatten()) != 0:
                 # print(len(np.array(total_link_urls).flatten()))
-                for i in range(10):
+                for i in range(10) if args.mode == "test" else range(0, 1):
                     if len(total_link_urls[i]) == 0:
                         continue
                     url = total_link_urls[i][0]
